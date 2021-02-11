3684f96d6ea92a084c48f51f02b93ca5c21d8253
sourceCodeISA <isabel.gogov1@libero.it>
2021-02-11 22:13:54 +0100

    esercizio completato

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -2,27 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;//includo Auth
+use Illuminate\Support\Facades\Mail; //includo Mail
+use App\Mail\TestMail; //includo TestMail
+
 use Illuminate\Http\Request;
+
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+    public function sendMail(Request $request) {
+        $data = $request -> validate([
+            'mailText' => 'required|min:5'
+        ]);
+
+        // dd($data);
+        
+        Mail::to(Auth::user() -> email)
+            -> send(new TestMail($data['mailText']));
+
+
+        return redirect() -> back();
+    }
+
     public function index()
-    {
+    {   
+       // dd(Auth::user());
+    //    $mail = (Auth::user() -> email); //ottengo l'email dell'ute collegato
+
+    //    Mail::to($mail) //mandiamo un email all'utente collegato 
+    //     -> send(new TestMail('Welcome from Laravel')); //mandando il messaggio contenuto nel TastMail
+
         return view('home');
     }
 }
